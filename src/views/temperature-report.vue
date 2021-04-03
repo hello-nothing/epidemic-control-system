aa036ec440d96484aaa5f1eea573f87d3c5fa47b
魏蕊 <1471059456@qq.com>
2021-04-03 20:36:31 +0800

    体温上报联调

--- src/views/temperature-report.vue
+++ src/views/temperature-report.vue
@@ -7,16 +7,17 @@
       placeholder="请输入您的温度"
     ></el-input>
     <el-button type="primary" @click="confirmSubmit">确定上报</el-button>
-    <el-button v-if="temperature" @click="edit">编辑</el-button>
+    <!-- <el-button v-if="temperature" @click="edit">编辑</el-button> -->
   </div>
 </template>
 <script>
+import api from "../api/api";
 export default {
   name: "temperatureReport",
   data() {
     return {
       temperature: "",
-      userInfo: {}
+      userInfo: {},
     };
   },
   mounted() {
@@ -34,19 +35,40 @@ export default {
         return;
       }
       const params = {
-        useeId: this.userInfo.userId,
-        temperature: this.temperature
+        userId: this.userInfo.userId,
+        temperature: this.temperature,
       };
+      api.saveTemperature(params).then((res) => {
+        console.log(res);
+        const result = res.data;
+        if (result.status === 200) {
+          this.$message.success("上报成功！");
+        } else {
+          this.$message.warning(result.message);
+        }
+      });
     },
     // 编辑
-    edit() {
-      if (!this.temperature) {
-        this.$message.warning("请输入您的体温！");
-        return;
-      }
-      const params = {};
-    }
-  }
+    // edit() {
+    //   if (!this.temperature) {
+    //     this.$message.warning("请输入您的体温！");
+    //     return;
+    //   }
+    //   const params = {
+    //     userId: this.userInfo.userId,
+    //     temperature: this.temperature,
+    //   };
+    //   api.editTemperature(params).then((res) => {
+    //     console.log(res);
+    //     const result = res.data;
+    //     if (result.status === 200) {
+    //       this.$message.success("编辑成功！");
+    //     } else {
+    //       this.$message.warning(result.message);
+    //     }
+    //   });
+    // },
+  },
 };
 </script>
 <style lang="scss" scoped>
